29ced2f26d13472ad7fabfee4aa76290ddf62e76
Ciaran <ciaranschutte@gmail.com>
2016-07-22 21:34:35 -0400

    Removed remarkable. Added redux. Added folders for redux structure

---
+++ app/components/editor.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Editor extends React.Component {
+  render() {
+    return (
+      <div className="editor">
+        <div className="content">
+          <textarea></textarea>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Editor;
